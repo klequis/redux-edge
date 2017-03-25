88c7d75c5fe01fc9d39add1e30843e0312554973
klequis <klequis@gmail.com>
2017-03-24 21:42:41 -0700

    update

--- ch05.mutate/10/webpack.config.js
+++ ch05.mutate/10/webpack.config.js
@@ -12,9 +12,21 @@ const resolve = {
 };
 
 const scriptLoader = {
-  loader: 'babel',
+  loader: 'babel-loader',
   include: path.resolve(__dirname, 'source'),
   test: /\.jsx$|\.js$/,
+};
+
+const devServer = {
+  historyApiFallback: true,
+  proxy: {
+    '/api*': 'http://localhost:8181',
+  },
+  stats: 'errors-only',
+  overlay: {
+    errors: true,
+    warnings: true,
+  },
 };
 
 module.exports = {
@@ -23,4 +35,5 @@ module.exports = {
   output,
   resolve,
   module: { loaders: [ scriptLoader ] },
+  devServer,
 };
